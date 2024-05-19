77d8fc53a847c26d8d51f6dca5361ea021b6c0ff
ngagne <ngagne966@gmail.com>
2024-05-19 17:16:25 -0400

    Initial commit

---
+++ Start_Satdump_webpage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Activate the virtual environment
+cd /home/ngagne/PycharmProjects/Satdump_webpage/
+source /home/ngagne/PycharmProjects/Satdump_webpage/venv/bin/activate
+
+#Run the Python script
+python3 /home/ngagne/PycharmProjects/Satdump_webpage/main.py
